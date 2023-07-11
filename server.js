0564d9ab70395aa5412da57c3b2a947c2bdbe339
Ellichept <91894050+Ellichept@users.noreply.github.com>
2023-07-11 09:30:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -120,9 +120,10 @@ app.get('/events-registration',(req,res,next)=> {
 // End of routes
 
 const port = 5501; // Set the desired port number
+const host = '0.0.0.0'; // Set the desired host address or use 'localhost' if applicable
 
-app.listen(port, '127.0.0.1', () => {
-  console.log(`Application started and listening on http://127.0.0.1:${port}/`);
+app.listen(port, host, () => {
+  console.log(`Application started and listening on http://${host}:${port}/`);
 });
 
 app.post('/events', async(req,res)=>{
